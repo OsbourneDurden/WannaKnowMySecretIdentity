75df43e7a61eddb565f74900dadee0ea4c9a14cf
chiptuned <pro@vincent.maison>
2016-01-31 13:09:57 +0100

    Ajout de workspace.mat, minimisation des erreur utilisateur
    Lorsque l'on extrait des points 3D l'erreur peut être grande entre les
    points et les points re-projetés. A chaque passe on peut essayer de
    réduire cette différence (sur une coordonnée d'un pixel) et re-calculer
    le tout pour faire baisser l'erreur de projection. De cette manière on
    améliore la précision du système.

--- creation_workspace.m
+++ creation_workspace.m
@@ -1,33 +1,43 @@
 function [ points_monde, points_image_D, points_image_G ] = creation_workspace()
+%%  Mise en place des points 3D et des points_images de la mire gauche et droite
+%
+% INPUT : []
+% OUTPUT : [points_monde, points_image_D, points_image_G] où points_monde 
+% est une matrice N*3 de points dans le monde, et points_image_D ou G sont
+% des matrices de N*2  points dans le plan image correspondant pour D à la
+% vue droite et G à la vue gauche
+%
+% On va charger le workspace s'il à déjà été crée, sinon on demande à
+% l'utilisateur d'extraire des points de la mire en vue droite, puis on
+% cherche à ré-extraire ces mêmes points dans la vue gauche. Enfin on
+% minimise l'erreur de l'utilisateur (précision de la sélection des pixels)
+% et on enregistre le résultat dans un fichier workspace.mat
 
 if exist('workspace.mat','file')
-    load('workspace.mat');
-else
-    cond=2;
-    while cond > 1
-        %% Initialisation des variables
-        fichier_mire='img/vue droite0.png';
-        fichier_mire2='img/vue gauche0.png';
-
-        %% Extraction des points 3D et des points image
-        [ points_monde, points_image_D ] = extraction_points_mire( fichier_mire );
-        [ ~, points_image_G ] = extraction_points_mire( fichier_mire2 , points_monde );
-
-        %% Résolution du problème
-        [ matrice_projection_D ] = calcul_matrice_projection_dlt( points_monde , points_image_D );
-        [ points_image_D_projete ] = projection_points( points_monde , matrice_projection_D );
-        [ erreur_projection_D ] = calcul_erreur_projection( points_image_D , points_image_D_projete );
-        [ matrice_projection_G ] = calcul_matrice_projection_dlt( points_monde , points_image_G );
-        [ points_image_G_projete ] = projection_points( points_monde , matrice_projection_G );
-        [ erreur_projection_G ] = calcul_erreur_projection( points_image_G , points_image_G_projete );
-
-        %% Test de la précision du système à enregistrer
-        cond = erreur_projection_D + erreur_projection_G;
-        
-        if cond >= 1
-            disp('Les points sont pas bons, réesayez :-D');
+    K = menu('Fichier workspace.mat existe déjà', ...
+        ['Refaire les points 3D, et points_image de la mire,', ... 
+        'vue gauche et droite'], ...
+        'Charger les données existantes');
+    if K == 1;
+        if exist('workspace.mat.old','file')
+            delete('workspace.mat.old');
         end
-        
+        movefile('workspace.mat','workspace.mat.old')
+        [ points_monde, points_image_D, points_image_G ] = creation_workspace();
     end
+    
+else
+    %% Initialisation des variables
+    fichier_mire='img/vue droite0.png';
+    fichier_mire2='img/vue gauche0.png';
+
+    %% Extraction des points 3D et des points image
+    [ points_monde, points_image_D ] = extraction_points_mire( fichier_mire );
+    [ ~, points_image_G ] = extraction_points_mire( fichier_mire2 , points_monde );
+
+    %% Résolution du problème
+    [ points_image_D ] = minimisation_erreur_projection( points_monde, points_image_D );
+    [ points_image_G ] = minimisation_erreur_projection( points_monde, points_image_G );
+
     save( 'workspace.mat', 'points_monde', 'points_image_D', 'points_image_G' );
 end
---
+++ minimisation_erreur_projection.m
@@ -0,0 +1,53 @@
+function [ points_image ] = minimisation_erreur_projection( points_monde, points_image )
+%% Minimisation de l'erreur de projection d'un problème
+%
+% INPUT : [points_monde, points_image] où points_monde est une matrice
+% N*3 de points dans le monde, et points_image est une matrice de N*2 
+% points dans le plan image
+% OUTPUT : points_image, une matrice de N*2 points dans le plan image
+%
+% On cherche à minimiser la différence entre les points_image et les
+% points images projetés par la matrice de projection de ces points. On
+% minimise donc la différence d'un certain pas et on le fait tant qu'on
+% arrive à minimiser l'erreur de projection de cette manière.
+
+%% Initialisation des variables
+pi=points_image;
+step=0.01;
+err=1000;
+[ matrice_projection ] = calcul_matrice_projection_dlt( points_monde , pi );
+[ pip ] = projection_points( points_monde , matrice_projection );
+[ new_err ] = calcul_erreur_projection( pi , pip );
+disp(['Erreur de projection avant minimisation : ', num2str(new_err), ' pixels']);
+
+%% Minimisation
+while err >= new_err
+    
+    [ matrice_projection ] = calcul_matrice_projection_dlt( points_monde , pi );
+    [ pip ] = projection_points( points_monde , matrice_projection );
+    err=new_err;
+    [ new_err ] = calcul_erreur_projection( pi , pip );
+    
+    if err >= new_err
+        %% Recherche du pixel le plus éloigné 
+        diff=abs(mean(pip-pi));
+        indice_pixel=find( max(diff) == diff);
+        
+        difference_pixel=pip(:,indice_pixel)-pi(:,indice_pixel);
+        eloignement_pixel=abs(difference_pixel);
+        
+        %% Recherche de la coordonée à réduire d'un certain pas
+        coordonnee = find(max(eloignement_pixel) == eloignement_pixel);
+        pi(coordonnee, indice_pixel)=pi(coordonnee, indice_pixel) + ...
+            sign(difference_pixel(coordonnee))*step;
+        
+    else
+        % Sinon on ne peut plus réduire, on arrête la boucle
+        break;
+    end
+    
+    
+end
+disp(['Erreur de projection après minimisation : ', num2str(new_err), ' pixels']);
+points_image=pi;
+end
